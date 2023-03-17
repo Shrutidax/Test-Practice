11d66981d8f21b5c2511daa2be2b754f90dcbcbd
Shrutidax <shruti.sahu@data-axle.com>
2023-03-17 16:15:25 +0530

    Asynchronous functions testing

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Test-Practice
+# 1. Simple JavaScript Tests ✓
+# 2. Using Functions and References ✓
+# 3. Using asynchronous functions ✓
+# 4. Using globals ✓
--- test4.js
+++ test4.js
@@ -1,21 +1,21 @@
-const product = (a,b) => a * b
-const div = (a,b) => a / b
+const productAsync = (a,b) => a * b
+const divAsync = (a,b) => a / b
 
-test(`Multiplication`, () => {
-    result = product (7, 8)
+test(`Multiplication`, async () => {
+    result = await productAsync (7, 8)
     expected = 56
     weGot(result).weWant(expected)
 })
 
-test(`Division`, () => {
-    result = div (12, 3)
-    expected = 3
+test(`Division`, async () => {
+    result = await divAsync (12, 3)
+    expected = 4
     weGot(result).weWant(expected)
 })
 
-function test (title , callback) {
+async function test (title , callback) {
     try {
-        callback()
+        await callback()
         console.log(`✓ ${title}`)
     }
     catch (error) {
@@ -31,3 +31,5 @@ function weGot (actual) {
         }
     }
 }
+
+// ✓   ✕
